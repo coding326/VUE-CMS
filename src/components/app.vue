0840432114e19d95a5403946375e812a91910edd
coding326 <wjp326@gmail.com>
2022-01-16 02:01:55 -0500

    Vue项目进度1

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,6 +1,34 @@
 <template>
-    <div>
+    <div class="app-container">
+   <!--这里放一个header-->
+        <mt-header fixed title="黑马程序员.Vue项目">
+
+            <span slot="left">
+    <mt-button icon="back">返回</mt-button>
+  </span>
+        </mt-header>
     <h3>这是APP根组件</h3>
+ 
+ <!--tabbar 区域-->
+
+    <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">会员</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">0</span></span>
+				<span class="mui-tab-label">购物车</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-search"></span>
+				<span class="mui-tab-label">搜索</span>
+			</a>
+		</nav>
     </div>
 </template>
 
@@ -13,6 +41,10 @@ export default {
 
 <!--这里写的样式，要符合SCSS语法，并且样式是当前组件私有的
    ，不会成为全局的样式，也不会应用给子组件-->
-<style lang="sass" scoped>
+ 
+ <style lang="scss" scoped>
+ .app-container{
+     padding-top: 40px;
+ }
+ </style>
 
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 //项目的JS打包入口文件
 //1导入vue
 import Vue from 'vue'
+//全局配置MintUI组件库
+import MintUI from 'mint-ui'
+//导入mint-ui 的样式表
+import 'mint-ui/lib/style.css'
+//安装MintUI
+Vue.use(MintUI)
+
+//导入MUI的样式表(路径)
+import "../lib/mui/css/mui.min.css"
+//导入MUI扩展样式表(购物车图标用到)
+import "../lib/mui/css/icons-extra.css"
 //导入根组件
 import App from './components/app.vue'
 
